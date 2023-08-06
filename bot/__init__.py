072a9ec8bdbbd42c1f63b71ee0fd974f8fbd7d2a
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-08-06 03:48:01 -0700

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -794,7 +794,7 @@ else:
 
 
 log_info("Creating client from BOT_TOKEN")
-bot = tgClient('bot', TELEGRAM_API, TELEGRAM_HASH, bot_token=BOT_TOKEN,, workers=1000,
+bot = tgClient('bot', TELEGRAM_API, TELEGRAM_HASH, bot_token=BOT_TOKEN, workers=1000,
                parse_mode=enums.ParseMode.HTML).start()
 bot_loop = bot.loop
 bot_name = bot.me.username
