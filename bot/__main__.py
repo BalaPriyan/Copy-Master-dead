4621365c6b203e823f00615c1d5b1984b608f0f5
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-08-09 03:31:03 -0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -163,11 +163,11 @@ help_string = f'''
 async def bot_help(client, message):
     buttons = ButtonMaker()
     user_id = message.from_user.id
-    buttons.ibutton('Basic', f'wzmlx {user_id} guide basic')
-    buttons.ibutton('Users', f'wzmlx {user_id} guide users')
-    buttons.ibutton('Mics', f'wzmlx {user_id} guide miscs')
-    buttons.ibutton('Owner & Sudos', f'wzmlx {user_id} guide admin')
-    buttons.ibutton('Close', f'wzmlx {user_id} close')
+    buttons.ibutton('Basic', f'{user_id} guide basic')
+    buttons.ibutton('Users', f'{user_id} guide users')
+    buttons.ibutton('Mics', f'{user_id} guide miscs')
+    buttons.ibutton('Owner & Sudos', f'{user_id} guide admin')
+    buttons.ibutton('Close', f'{user_id} close')
     await sendMessage(message, "⚙ <b><i>Help Guide Menu!</i></b>\n\n<b>NOTE: <i>Click on any CMD to see more minor detalis.</i></b>", buttons.build_menu(2))
 
 
